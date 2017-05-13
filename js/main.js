8fd8c4ad997eaec45b5aa571f32dfd636bfcd1dd
laerisx9795 <laerisx9795@gmail.com>
2017-05-13 16:45:43 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("SR");
+var englishScoreCollection = db.collection('score');
+var studentCollection = db.collection('student');
+function show(){
+  for(var i=0;i<studentCollection.find().length;i++){
+    $("tbody").text("");
+	  var name = "<tr><td>"+studentCollection.find().number+"</td><td><input type='text' class='inputs'></td></tr>";
+	  $("tbody").append(name);
+	}
+}
+$("#record").on("click",show);
