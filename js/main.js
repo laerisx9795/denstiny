c90bc617bfb04a5cbff734e5ca728102973619a8
laerisx9795 <laerisx9795@gmail.com>
2017-05-13 17:17:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-function show(){
-  for(var i=0;i<studentCollection.find().length;i++){
-    $("tbody").text("");
-	  var name = "<tr><td>"+studentCollection.find().number+"</td><td><input type='text' class='inputs'></td></tr>";
-	  $("tbody").append(name);
-	}
-}
-$("#create").on("click",show);
+
